b4a6004132ce7d58a299dacb30d8f9e82ba8a8f7
vzwGrey <vzwgrey@gmail.com>
2022-06-13 14:06:55 +0200

    feat: move from Sandbox to Application, implement async tcp connection

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,8 +1,11 @@
-use std::io::{Read, Write};
-use std::net::TcpStream;
+use std::sync::Arc;
 
-use iced::pure::{button, column, row, text, text_input, Element, Sandbox};
-use iced::{Alignment, Settings};
+use iced::pure::{button, column, row, text, text_input, Application, Element};
+use iced::{Alignment, Command, Settings};
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 
 const TOGGLE_COMMAND: u8 = 0xAA;
 const ACKNOWLEDGE_COMMAND: u8 = 0x06;
@@ -22,7 +25,7 @@ struct ConnectionFailedState {
 
 #[derive(Debug)]
 struct ConnectedState {
-    socket: TcpStream,
+    socket: Arc<Mutex<TcpStream>>,
 }
 
 #[derive(Debug)]
@@ -32,56 +35,50 @@ enum App {
     Connected(ConnectedState),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum Message {
     AddressChanged(String),
     PortChanged(String),
-    RetryConnect,
     Connect,
+    RetryConnect,
+    Connected(tokio::io::Result<tokio::net::TcpStream>),
     ToggleLed,
+    ToggledLed(Result<(), String>),
 }
 
-impl Sandbox for App {
-    type Message = Message;
+impl Clone for Message {
+    fn clone(&self) -> Self {
+        match self {
+            Message::AddressChanged(address) => Message::AddressChanged(address.clone()),
+            Message::PortChanged(port) => Message::PortChanged(port.clone()),
+            Message::Connect => Message::Connect,
+            Message::RetryConnect => Message::RetryConnect,
+            Message::Connected(_result) => {
+                panic!("Message::clone: Cannot clone Message::Connected")
+            }
+            Message::ToggleLed => Message::ToggleLed,
+            Message::ToggledLed(r) => Message::ToggledLed(r.clone()),
+        }
+    }
+}
 
-    fn new() -> Self {
-        Self::Disconnected(DisconnectedState::default())
+impl Application for App {
+    type Executor = iced::executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            Self::Disconnected(DisconnectedState::default()),
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         "LED Control".to_string()
     }
 
-    fn update(&mut self, msg: Self::Message) {
-        if let Message::Connect = msg {
-            take_mut::take(self, |state| {
-                if let Self::Disconnected(state) = state {
-                    let address = state.address.as_str();
-                    let port = if let Ok(port) = state.port.parse::<u16>() {
-                        port
-                    } else {
-                        return Self::ConnectionFailed(ConnectionFailedState {
-                            address: state.address,
-                            port: state.port,
-                            reason: "Invalid port".to_string(),
-                        });
-                    };
-
-                    match TcpStream::connect((address, port)) {
-                        Ok(socket) => Self::Connected(ConnectedState { socket }),
-                        Err(e) => Self::ConnectionFailed(ConnectionFailedState {
-                            address: state.address,
-                            port: state.port,
-                            reason: e.to_string(),
-                        }),
-                    }
-                } else {
-                    unreachable!()
-                }
-            });
-            return;
-        }
-
+    fn update(&mut self, msg: Self::Message) -> Command<Message> {
         match self {
             Self::Disconnected(state) => match msg {
                 Message::AddressChanged(address) => {
@@ -90,24 +87,56 @@ impl Sandbox for App {
                 Message::PortChanged(port) => {
                     state.port = port;
                 }
+                Message::Connect => {
+                    let go = || -> eyre::Result<_> {
+                        let address = state.address.clone();
+                        let port = state.port.parse::<u16>()?;
+                        Ok(Command::perform(
+                            TcpStream::connect((address, port)),
+                            Message::Connected,
+                        ))
+                    };
+                    if let Ok(cmd) = go() {
+                        return cmd;
+                    } else {
+                        *self = Self::ConnectionFailed(ConnectionFailedState {
+                            address: state.address.clone(),
+                            port: state.port.clone(),
+                            reason: "Invalid port".to_string(),
+                        });
+                    };
+                }
+                Message::Connected(socket) => {
+                    *self = match socket {
+                        Ok(socket) => Self::Connected(ConnectedState {
+                            socket: Arc::new(Mutex::new(socket)),
+                        }),
+                        Err(e) => Self::ConnectionFailed(ConnectionFailedState {
+                            address: state.address.clone(),
+                            port: state.port.clone(),
+                            reason: e.to_string(),
+                        }),
+                    }
+                }
                 _ => unreachable!(),
             },
             Self::ConnectionFailed(_state) => match msg {
-                Message::RetryConnect => *self = Self::new(),
+                Message::RetryConnect => *self = Self::Disconnected(DisconnectedState::default()),
                 _ => unreachable!(),
             },
             Self::Connected(state) => match msg {
                 Message::ToggleLed => {
-                    let written_len = state.socket.write(&[TOGGLE_COMMAND]).unwrap();
-                    assert_eq!(written_len, 1);
-
-                    let mut ack = [0];
-                    state.socket.read_exact(&mut ack).unwrap();
-                    assert_eq!(ack, [ACKNOWLEDGE_COMMAND]);
+                    return Command::perform(
+                        send_toggle_command(state.socket.clone()),
+                        Message::ToggledLed,
+                    );
                 }
+                Message::ToggledLed(_e) => {}
                 _ => unreachable!(),
             },
         }
+
+        Command::none()
     }
 
     fn view(&self) -> Element<Self::Message> {
@@ -142,15 +171,12 @@ impl Sandbox for App {
                 )))
                 .push(button("Back").on_press(Message::RetryConnect))
                 .into(),
-            Self::Connected(state) => {
+            Self::Connected(_state) => {
                 let message_input = row().push(button("Toggle LED").on_press(Message::ToggleLed));
 
                 column()
                     .padding(20)
-                    .push(text(format!(
-                        "Connected to `{}`.",
-                        state.socket.peer_addr().unwrap()
-                    )))
+                    .push(text("Connected."))
                     .push(message_input)
                     .into()
             }
@@ -158,6 +184,24 @@ impl Sandbox for App {
     }
 }
 
-pub fn main() -> iced::Result {
+async fn send_toggle_command(socket: Arc<Mutex<TcpStream>>) -> Result<(), String> {
+    let mut socket = socket.lock().await;
+
+    let written_len = socket
+        .write(&[TOGGLE_COMMAND])
+        .await
+        .map_err(|e| e.to_string())?;
+    assert_eq!(written_len, 1);
+
+    let mut ack = [0];
+    socket
+        .read_exact(&mut ack)
+        .await
+        .map_err(|e| e.to_string())?;
+    assert_eq!(ack, [ACKNOWLEDGE_COMMAND]);
+    Ok(())
+}
+
+fn main() -> iced::Result {
     App::run(Settings::default())
 }
